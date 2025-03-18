1e0c8b21c94a4c0758f8eb02d7c5e5a60cc9d0cf
Julia Weppler <jnweppler@gmail.com>
2025-03-17 23:15:59 -0400

    Fixing secrets issue

--- services/oauth.py
+++ services/oauth.py
@@ -2,9 +2,10 @@ from urllib.parse import urlencode
 import streamlit as st
 def get_inoreader_auth_url():
     params = {
-        "client_id": st.secrets["INOREADER_CLIENT_ID"],
+        "client_id": st.secrets["oauth_client_id"],
         "redirect_uri": st.secrets["REDIRECT_URI"],
         "response_type": "code",
         "scope": st.secrets["SCOPE"]
     }
+    print("PARAMS:", params)
     return f"{st.secrets['AUTHORIZATION_URL']}?{urlencode(params)}"
