58cf9e952d6abfa31f6ff1d87c4af1e7f5b6758b
Julia Weppler <jnweppler@gmail.com>
2025-03-17 23:21:55 -0400

    Update oauth.py

--- services/oauth.py
+++ services/oauth.py
@@ -5,7 +5,8 @@ def get_inoreader_auth_url():
         "client_id": st.secrets["oauth_client_id"],
         "redirect_uri": st.secrets["REDIRECT_URI"],
         "response_type": "code",
-        "scope": st.secrets["SCOPE"]
+        "scope": st.secrets["SCOPE"],
+        "state": st.secrets["inoreader_key"]
     }
     print("PARAMS:", params)
     return f"{st.secrets['AUTHORIZATION_URL']}?{urlencode(params)}"
