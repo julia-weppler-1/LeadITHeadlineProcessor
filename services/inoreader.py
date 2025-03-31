9b8800ddb419095046919cafdf1baec43c8903ef
Julia Weppler <jnweppler@gmail.com>
2025-03-31 13:25:09 -0400

    Update inoreader.py

--- services/inoreader.py
+++ services/inoreader.py
@@ -6,7 +6,6 @@ from utils.read_json import parse_inoreader_feed
 from newspaper import Article
 import re
 import urllib.parse
-from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 
 
